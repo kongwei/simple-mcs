79b7552ac47ef3cc3261396e5c36d20f0f9d0ab8
kongwei <13851894043@139.com>
2025-04-29 09:04:13 +0800

    Delete application directory

--- application/pktsolve/include/psNcuPktParse.h
+++
@@ -1,8 +0,0 @@
-#ifndef PS_NCU_PKT_PARSE_H_
-#define PS_NCU_PKT_PARSE_H_
-
-#include "psUpfCommon.h"
-#include "xdb_pfu_dyntbl_acc.h"
-WORD32 psNcuParsePktInfo(T_MediaProcThreadPara* ptMediaProcThreadPara, VOID *ptPktData);
-BOOLEAN psNcuIsPktHasSynAckFlg(T_psNcuPktDesc *ptPktDesc);
-#endif
--- application/report/include/psNcuReportSBCProc.h
+++
@@ -1,8 +0,0 @@
-#ifndef PS_NCU_REPORT_SBC_PROC_H_
-#define PS_NCU_REPORT_SBC_PROC_H_
-
-#include "ps_ncu_dataApp.h"
-#include "ps_ncu_stream.h"
-void psNcuReportToUpmProc(T_psNcuDaAppCtx * ptDaAppCtx, T_psNcuFlowCtx *ptStmAddr, WORD16 threadID, BYTE bRptType);
-
-#endif
